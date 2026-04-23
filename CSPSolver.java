a8fc4a394c673ac5cf0d4674f94f824dc887113a
ibacas1 <irbacas@gmail.com>
2026-04-22 21:42:50 -0500

    Update CSPSolver.java

--- CSPSolver.java
+++ CSPSolver.java
@@ -4,3 +4,12 @@ import game.Board;
 import game.Tile;
 import java.util.ArrayList;
 import java.util.List;
+
+//handles constraint satisfaction problem logic for the agent 
+public class CSPSolver { 
+private final Board board; 
+
+public CSPSolver (Board board){
+  this.board = board; 
+}
+  
