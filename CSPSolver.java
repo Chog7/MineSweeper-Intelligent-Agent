0eebd2818c66d2c5837fba05b23dda959c66534b
ibacas1 <irbacas@gmail.com>
2026-04-23 12:09:10 -0500

    Update CSPSolver.java

--- CSPSolver.java
+++ CSPSolver.java
@@ -24,4 +24,17 @@ public boolean solveSingleStep() {
 
     //we only care about revealed clues greater than 0 
     if (tile.isRevealed() && tile.getAdjacentMines() > 0) {
-      
+      List<Tile> neighbors = board.getNeighbors(x, y); 
+      int hiddenCount = 0; 
+      int flaggedCount = 0; 
+      List<Tile> hiddenNeighbors = new ArrayList<>();
+
+      for (Tile neighbor : neighbors) {
+        if (neighbor.isFlagged()) {
+          flaggedCount++;
+          } else if (!neighbor.isRevealed()) {
+          hiddenCount++;
+          hiddenNeighbors.add(neighbor);
+        }
+      }
+
